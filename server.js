6e64f2e0d6ae9065d63d3316e0444ed94b055279
Iskren Alexandrov <iskren.alexandrov@gmail.com>
2020-08-25 09:22:09 +0300

    rollback

--- server.js
+++ server.js
@@ -5,17 +5,16 @@ md5 = require('js-md5');
 const app = express();  
 app.use(express.static('data/images'));
 
-const port = 80;
+const port = 8000;
 
 //require('./app/routes')(app, {});
 app.use(bodyParser.urlencoded( { extended: true } ))
 
 app.post('/', (req, res) => {
-    let hash = req.body.hash.toString();
     let data = req.body.image.toString();
     let commaPos = data.search(",");
     let base64Data = data.substring(commaPos+1);
-    // let hash = md5(base64Data);
+    let hash = md5(base64Data);
     require("fs").writeFile('./data/images/'+hash+'.png', base64Data, 'base64', function(err) {
     //console.log(err);
     });
